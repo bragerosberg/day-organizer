75e6348818f982fa56917f7ad680632bb3ed6991
Brage Rosberg <brage70@gmail.com>
2020-08-26 23:49:15 +0200

    Heroku Deployment

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

c4960fbe7bb5ce32166c6b70f8b9d9e9f2c9eac3
Brage Rosberg <brage70@gmail.com>
2020-08-27 11:57:36 +0200

    Heroku deploy

---
+++ public/build/precache-manifest.ec87b74ccd1b222d3189162ac8cbbec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7a14452503a6a83f5ad764901d0857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ab87a88bd44461df9b1",
+    "url": "/static/css/main.a3de6df6.chunk.css"
+  },
+  {
+    "revision": "e937a78aaffedb302016",
+    "url": "/static/js/2.47a07c5d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.47a07c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab87a88bd44461df9b1",
+    "url": "/static/js/main.3837415c.chunk.js"
+  },
+  {
+    "revision": "252c54e5682a184ebd9c",
+    "url": "/static/js/runtime-main.19184806.js"
+  }
+]);

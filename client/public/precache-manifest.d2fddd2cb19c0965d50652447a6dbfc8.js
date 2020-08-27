11ff96e7d6ce24ce047bd67fe6f3dc5336836a8f
Brage Rosberg <brage70@gmail.com>
2020-08-27 18:38:13 +0200

    Built the new changes, deploying to Heroku

---
+++ client/public/precache-manifest.d2fddd2cb19c0965d50652447a6dbfc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783093250c1fcf72997c0334dc2ed540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0c4b0364007abc9bc91",
+    "url": "/static/css/main.d75ba7cc.chunk.css"
+  },
+  {
+    "revision": "e937a78aaffedb302016",
+    "url": "/static/js/2.47a07c5d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.47a07c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c4b0364007abc9bc91",
+    "url": "/static/js/main.3837415c.chunk.js"
+  },
+  {
+    "revision": "252c54e5682a184ebd9c",
+    "url": "/static/js/runtime-main.19184806.js"
+  }
+]);
